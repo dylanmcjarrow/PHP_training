15796ac08c7092ed3f004af0fec0c838d303751c
Dylan Mc Jarrow <d.mcjarrow@gmail.com>
2020-12-01 11:13:43 +0200

    DEC-1

---
+++ foundation_task2.php
@@ -0,0 +1,40 @@
+<?php
+function fibonacci_loop($num1=1,$num2=1) {
+
+    while (true){
+        $result = $num1+$num2;
+        $num1 = $num2;
+        $num2 = $result;
+        if($result<=34){
+            echo $result."\n";
+        }else{
+            return;
+        }
+
+    }
+
+}
+function fibonacci_recursive($num1=1,$num2=1) {
+    $result = $num1+$num2;
+    if($result>34){
+        return;
+    }else{
+        $num1 = $num2;
+        $num2 = $result;
+        echo $result."\n";
+        fibonacci_recursive($num1,$num2);
+    }
+}
+
+$num1=1;
+$num2=1;
+echo "Fibonacci loop \n";
+echo $num1."\n";
+echo $num2."\n";
+echo fibonacci_loop($num1,$num2);
+
+echo "Fibonacci recursive \n";
+echo $num1."\n";
+echo $num2."\n";
+echo fibonacci_recursive($num1,$num2);
+?>
