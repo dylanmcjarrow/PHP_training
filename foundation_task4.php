20fede5062f40e10b3ef99260d51cb604413fb97
Dylan Mc Jarrow <d.mcjarrow@gmail.com>
2020-12-01 12:15:33 +0200

    DEC-1

--- foundation_task4.php
+++ foundation_task4.php
@@ -6,20 +6,36 @@ class ItemOwners
     {
         //TODO: Implement this
         $uniqueOwners = [];
-        foreach ($items as $item) {
-            if (!in_array($item, $uniqueOwners)) {
-
+        foreach ($items as  $item => $owner) {
+            if (!in_array($owner, $uniqueOwners)) {
+                array_push($uniqueOwners,$owner);
             }
-
         }
-        return null;
+        $groupedByOwners =[];
+        sort($uniqueOwners);
+
+        foreach ($uniqueOwners as $owner1){
+            $temp = [];
+            foreach ($items as  $item => $owner2) {
+              if($owner1 == $owner2){
+                  array_push($temp,$item);
+              }
+            }
+            $groupedByOwners[$owner1] = $temp;
+        }
+        return $groupedByOwners;
     }
 }
 
 $items = array(
-    "Baseball Bat" => "John",
+    "Cricket Bat" => "John",
+    "Golf Bat" => "John",
+    "Tennis Bat" => "John",
     "Golf ball" => "Stan",
-    "Tennis Racket" => "John"
+    "Tennis Racket" => "John",
+    "rugby ball" => "Stan",
+    "Soccer Ball" => "Gary",
+    "hockey ball" => "Stan"
 );
 echo json_encode(ItemOwners::groupByOwners($items));
 
