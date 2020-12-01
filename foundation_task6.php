74a9cb7964af2b740b185b702e7f4798558a9974
Dylan Mc Jarrow <d.mcjarrow@gmail.com>
2020-12-01 16:06:15 +0200

    DEC-1

--- foundation_task6.php
+++ foundation_task6.php
@@ -2,21 +2,48 @@
 
 include "foundation_task6_class.php";
 
+$time_start = microtime(true);
+
 $crudObj = new PersonCRUD("foundation_task6");
 
+//$personObj = [
+//    "name"=>"billy",
+//    "surname"=>"bob",
+//    "dateOfBirth"=>"1997-10-10",
+//    "email"=>"billybob@getrekt.com",
+//    "age"=>"30"
+//];
 $personObj = [
-    "name"=>"billy",
-    "surname"=>"bob",
-    "dateOfBirth"=>"1997-10-10",
-    "email"=>"billybob@getrekt.com",
-    "age"=>"30"
+    "name" => "billy",
+    "surname" => "bob",
+    "dateOfBirth" => "1997-10-10",
+    "email" => "billybob@getrekt.com",
+    "age" => "30"
 ];
 
 
-$crudObj->createPerson("Person",$personObj);
+for($i = 0;$i < 10;$i++){
+    $crudObj->createPerson("Person",$personObj);
+}
 
 
+$result = $crudObj->loadPerson("Person", "name", "billy");
+
+foreach ($result as $row) {
+    foreach ($row as $key => $value)
+        echo $key . "=>" . $value . "\n";
+}
+
+$result = $crudObj->loadAllPeople("Person");
+foreach ($result as $row) {
+    foreach ($row as $key => $value)
+        echo $key . "=>" . $value . "\n";
+}
 
 
+$crudObj->deletePerson("Person", "name", "billy");
 
+$time_end = microtime(true);
+$execution_time = ($time_end - $time_start);
+echo 'Total Execution Time: '.$execution_time.' seconds';
 ?>
--- foundation_task6_class.php
+++ foundation_task6_class.php
@@ -6,7 +6,8 @@ class PersonCRUD
     public $conn;
     public $name_of_db;
 
-    function __construct($dbName){
+    function __construct($dbName)
+    {
 
         $this->name_of_db = $dbName;
 
@@ -16,11 +17,11 @@ class PersonCRUD
 
         $this->conn = new mysqli($servername, $username, $password);
 
-        $sql = "USE ".$dbName;
+        $sql = "USE " . $dbName;
         if ($this->conn->query($sql) === TRUE) {
-            echo "Database Conected successfully". "\n";
+            echo "Database Conected successfully" . "\n";
         } else {
-            echo "Error Conected database: " . $this->conn->error. "\n";
+            echo "Error Conected database: " . $this->conn->error . "\n";
         }
 
     }
@@ -28,20 +29,25 @@ class PersonCRUD
     public function createPerson($tablename, $stdObj)
     {
 
-        $sql = "INSERT INTO ".$tablename."(" .implode(',', array_keys($stdObj)).") VALUES ( '". implode('\',\'', array_values($stdObj)) . "');";
-        echo $sql."\n";
+        $sql = "INSERT INTO " . $tablename . "(" . implode(',', array_keys($stdObj)) . ") VALUES ( '" . implode('\',\'', array_values($stdObj)) . "');";
         if ($this->conn->query($sql) === TRUE) {
-            echo "Data inserted successfully". "\n";
+            echo "Data inserted successfully" . "\n";
         } else {
-            echo "Error inserting into database: " . $this->conn->error. "\n";
+            echo "Error inserting into database: " . $this->conn->error . "\n";
         }
 
 
     }
 
-    public function loadPerson()
+    public function loadPerson($tablename,$column, $value)
     {
-
+        $sql = "SELECT * FROM " . $tablename . " WHERE ".$column."= '" . $value . "';";
+        $result = $this->conn->query($sql);
+        $outArr = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($outArr,$row);
+        }
+        return $outArr;
     }
 
     public function savePerson()
@@ -49,19 +55,35 @@ class PersonCRUD
 
     }
 
-    public function deletePerson()
+    public function deletePerson($tablename,$column, $value)
     {
-
+        $sql = "DELETE FROM ".$tablename." WHERE ".$column."='".$value."';";
+        if ($this->conn->query($sql) === TRUE) {
+            echo "Data deleted successfully" . "\n";
+        } else {
+            echo "Error deleted into database: " . $this->conn->error . "\n";
+        }
     }
 
-    public function loadAllPeople()
+    public function loadAllPeople($tablename)
     {
-
+        $sql = "SELECT * FROM " . $tablename .";";
+        $result = $this->conn->query($sql);
+        $outArr = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($outArr,$row);
+        }
+        return $outArr;
     }
 
-    public function deleteAllPeople()
+    public function deleteAllPeople($tablename)
     {
-
+        $sql = "DELETE FROM ".$tablename.";";
+        if ($this->conn->query($sql) === TRUE) {
+            echo "Data deleted successfully" . "\n";
+        } else {
+            echo "Error deleted into database: " . $this->conn->error . "\n";
+        }
     }
 
 }
